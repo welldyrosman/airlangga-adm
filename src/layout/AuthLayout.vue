c870ec54f17460eb8719d45ef735538d30bf2512
Welldy Rosman <welldyrosman@gmail.com>
2022-01-28 09:47:51 +0700

    Initial commit

---
+++ src/layout/AuthLayout.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="main-content bg-default">
+    <!-- Navbar -->
+    <!-- Header -->
+    <div
+      style="background: linear-gradient(87deg, #1a6fff 0, #171a4d 100%) !important"
+      class="header bg-gradient-green py-lg-2"
+    >
+      <div class="container">
+        <div class="header-body text-center mb-7">
+          <div class="row justify-content-center">
+            <div class="col-lg-5 col-md-6">
+              <img
+                style="max-height: 170px; margin-bottom: 50px; margin-top: 50px"
+                :src="logo"
+                class="navbar-brand-img"
+                alt="..."
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="separator separator-bottom separator-skew zindex-100">
+        <svg
+          x="0"
+          y="0"
+          viewBox="0 0 2560 100"
+          preserveAspectRatio="none"
+          version="1.1"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <polygon class="fill-default" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+      </div>
+    </div>
+    <!-- Page content -->
+    <div class="container mt--8 pb-5">
+      <router-view></router-view>
+    </div>
+    <footer class="py-8"></footer>
+  </div>
+</template>
+<script>
+export default {
+  name: "auth-layout",
+  props: {
+    logo: {
+      type: String,
+      default: "img/brand/travel.png",
+      description: "Sidebar app logo",
+    },
+  },
+  data() {
+    return {
+      year: new Date().getFullYear(),
+      showMenu: false,
+    };
+  },
+};
+</script>
+<style></style>
---
+++ src/views/Login.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="row justify-content-center">
+    <div class="col-lg-5 col-md-7">
+      <div class="card shadow border-0" style="background-color: #000000a1 !important">
+        <div class="card-header bg-transparent pb-5">
+          <div class="text-muted text-center mt-2 mb-3">
+            <small>Sign in with</small>
+          </div>
+          <div class="btn-wrapper text-center">
+            <a href="#" class="btn btn-neutral btn-icon">
+              <span class="btn-inner--icon"
+                ><img src="img/icons/common/google.svg"
+              /></span>
+              <span class="btn-inner--text">Google</span>
+            </a>
+          </div>
+        </div>
+        <div class="card-body px-lg-5 py-lg-5">
+          <div class="text-center text-muted mb-4">
+            <small>Or sign in with credentials</small>
+          </div>
+          <form role="form">
+            <base-input
+              formClasses="input-group-alternative mb-3"
+              placeholder="Email"
+              addon-left-icon="ni ni-email-83"
+              v-model="model.email"
+            >
+            </base-input>
+
+            <base-input
+              formClasses="input-group-alternative mb-3"
+              placeholder="Password"
+              type="password"
+              addon-left-icon="ni ni-lock-circle-open"
+              v-model="model.password"
+            >
+            </base-input>
+            <div class="text-center">
+              <base-button @click="login" type="primary" class="my-4"
+                >Sign in</base-button
+              >
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="row mt-3">
+        <div class="col-6"></div>
+        <div class="col-6 text-right"></div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      model: {
+        email: "",
+        password: "",
+      },
+    };
+  },
+  methods: {
+    login() {
+      this.$router.push("/dashboard");
+    },
+  },
+};
+</script>
+<style></style>
